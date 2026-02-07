a5486953a7368c8486200a3255f7645ed5fc77f4
DENILWAR <manuelfresko7@gmail.com>
2026-02-07 22:50:34 +0100

    DNSSS

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.use(helmet({
 // CORS configurado
 const allowedOrigins = process.env.ALLOWED_ORIGINS
     ? process.env.ALLOWED_ORIGINS.split(',')
-    : ['http://localhost:5500', 'http://127.0.0.1:5500'];
+    : ['http://localhost:5500', 'http://127.0.0.1:5500', 'https://denilsonarnau.netlify.app'];
 
 app.use(cors({
     origin: function (origin, callback) {
